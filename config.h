38dae41b69fe083ffef5496fd25f3cfd34e755fc
just-carlod <carlod@disroot.org>
2020-06-24 22:25:37 +0200

    test commit

---
+++ config.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1; // -b  option
+static int centered = 1; // -c option
+static int min_width = 500;
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#e5e9f0","#2e3440" },
+	[SchemeSel] = { "#e5e9f0", "#b48ead" },
+	[SchemeOut] = { "#e5e9f0", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 15;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+static const unsigned int border_width = 2;
